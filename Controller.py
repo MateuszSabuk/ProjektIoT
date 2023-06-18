0d908f38bbf606d513a918f9bdcf7f186e3d860b
MateuszSabuk <mateusz@sabuk.pl>
2023-06-18 22:21:10 +0200

    Interrupts for controller

--- Controller.py
+++ Controller.py
@@ -3,9 +3,21 @@ import time  # Import time library
 
 
 class Controller:
-    def __init__(self, pin):
+    def __init__(self, pin, bouncetime, reading_start_function, reading_end_function):
         self.pin = pin
-        GPIO.setup(pin, GPIO.IN)  # Set pin as GPIO in
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(
+            pin,
+            GPIO.RISING,
+            callback=reading_start_function,
+            bouncetime=bouncetime * 1000,
+        )
+        GPIO.add_event_detect(
+            pin,
+            GPIO.FALLING,
+            callback=reading_end_function,
+            bouncetime=bouncetime * 1000,
+        )
 
     def get_reading(self):
         return GPIO.input(self.pin)
